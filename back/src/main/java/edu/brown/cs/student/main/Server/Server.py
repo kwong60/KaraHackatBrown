121673c206ac52d6f3c247c542cccc4bc9d3af65
Emily W. <emilywang188@gmail.com>
2024-02-03 02:24:25 -0500

    added checkweather endpoint

--- back/src/main/java/edu/brown/cs/student/main/Server/Server.py
+++ back/src/main/java/edu/brown/cs/student/main/Server/Server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)  # Enable Cross-Origin Resource Sharing (CORS) for all routes
@@ -9,11 +10,23 @@ parsed_loaded_csv = []
 parsed_loaded_json = {}
 state_code = {}
 
-@app.route('/loadcsv', methods=['GET'])
-def load_csv():
+@app.route('/checkweather', methods=['GET'])
+def check_weather():
     # Implement the logic for loading the CSV file
     # You can use request.args to get query parameters if needed
-    return jsonify({"message": "CSV loaded successfully"})
+    api_key=f"53b068ebdeed4756a3c64249240302" # do we want future predictions/what parameters should user be able to enter
+    request_url = f"http://api.weatherapi.com/v1/current.json?key=53b068ebdeed4756a3c64249240302&q=Providence&aqi=no"
+    response = connect(request_url)
+    body = response.json()
+    current = body["current"]
+    return str(current["is_day"])    
+    # return jsonify({"message": "CSV loaded successfully"})
+    
+
+@staticmethod
+def connect(request_url):
+    url_connection = requests.get(request_url)
+    return url_connection
 
 @app.route('/viewcsv', methods=['GET'])
 def view_csv():
