d5858b3485ffebc4d377d8a18433d2ea0161b798
Emily W. <emilywang188@gmail.com>
2024-02-03 23:11:47 -0500

    links to login

--- back/src/main/java/edu/brown/cs/student/main/Server/src/Register.js
+++ back/src/main/java/edu/brown/cs/student/main/Server/src/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import styles from "./styles.css";
 import title from "./images/title.png";
+import {Link} from "react-router-dom"
 
 export function Register() {
   const [newUsername, setNewUsername] = useState("");
@@ -27,15 +28,26 @@ export function Register() {
 
       // Assuming your Flask server responds with a success message
       if (response.data.status == "success") {
-        console.log("Registration successful");
+        document.getElementById("message_to_user").innerHTML =
+          "Registration successful! Please log in.";
+        document.getElementById("message_to_user").style.display = "block"
         // Redirect or perform other actions upon successful registration
+        document.getElementById("things_to_hide").style.display= "none"
+        document.getElementById("login_button").style.display="block"
       } else {
-        console.log(response.data)
-        console.log("Registration failed");
+        document.getElementById("message_to_user").innerHTML =
+          "That username is already taken! Try again!";
+        document.getElementById("message_to_user").style.display = "block";
+  
       }
     } catch (error) {
-      console.error("Error during registration:", error.message);
+      document.getElementById("message_to_user").innerHTML =
+        "Unexpected error, try another username!";
+      document.getElementById("message_to_user").style.display = "block";
+
     }
+
+
   };
 
   return (
@@ -44,41 +56,60 @@ export function Register() {
         <img src={title} alt="Title" />
       </h1>
       <h2>New User Registration</h2>
-      <div className="username">
-        <p style={{ color: "white" }}>
-          New Username:{" "}
-          <input
-            type="text"
-            value={newUsername}
-            onChange={(e) => setNewUsername(e.target.value)}
-          />
-        </p>
+      <div id="things_to_hide">
+        <div className="username">
+          <p style={{ color: "white" }}>
+            New Username:{" "}
+            <input
+              type="text"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+            />
+          </p>
+        </div>
+        <div className="password">
+          <p style={{ color: "white" }}>
+            New Password:{" "}
+            <input
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+          </p>
+        </div>
+        <div className="password_con">
+          <p style={{ color: "white" }}>
+            Confirm Password:{" "}
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </p>
+        </div>
+        <div className="submit_reg">
+          <button type="button" onClick={handleRegistration}>
+            Submit
+          </button>
+        </div>
       </div>
-      <div className="password">
-        <p style={{ color: "white" }}>
-          New Password:{" "}
-          
-          <input
-            type="password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-          />
-        </p>
+      <div>
+        <p
+          className="user_message"
+          id="message_to_user"
+          style={{ display: "none" }}
+        ></p>
       </div>
-      <div className="password_con">
-        <p style={{ color: "white" }}>
-          Confirm Password:{" "}
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </p>
-      </div>
-      <div className="submit_reg">
-        <button type="button" onClick={handleRegistration}>
-          Submit
-        </button>
+      <div>
+        <Link to="/login">
+          <button
+            type="button"
+            id="login_button"
+            style={{ display: "none" }}
+          >
+            Login
+          </button>
+        </Link>{" "}
       </div>
     </div>
   );
